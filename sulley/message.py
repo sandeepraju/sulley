7d52020faa2f0461dace67b21c557c51d9a0ac45
Sandeep Raju Prabhakar <me@sandeepraju.in>
2016-08-05 23:11:01 -0500

    message should only contain message

--- sulley/message.py
+++ sulley/message.py
@@ -1,10 +1,7 @@
 class Message(object):
-    # TODO: add uuid (use decorator?)
-    def __init__(self, from_number, text, provider, *args, **kwargs):
+    def __init__(self, from_number, text, *args, **kwargs):
         self.from_number = from_number
         self.text = text
-        self._provider = provider
 
-    def reply(self, text):
-        print 'sending message [{}] to {}'.format(text, self._from)
-        self._provider.send(self._from, text)
+    def __repr__(self):
+        return '[{} - {}]'.format(self.from_number, self.text)
